d3db86e7a1ceca11f308730feb441fea1310a24e
Nail Kamilov <38252272+Kamilovkz@users.noreply.github.com>
2022-05-29 13:00:11 +0600

    Update Contents.swift
    Update to v2

--- Contents.swift
+++ Contents.swift
@@ -1,49 +1,33 @@
-import UIKit
+import Foundation
 
-// Define main objects of game
-// In enum we will add a protocol "CaseIterable" for random chosing of steps of players
-enum GameObj: CaseIterable {
-    case Paper
-    case Rock
-    case Scissors
+enum Game: CaseIterable {
+    case Scissors, Rock, Paper
 }
 
-//Declare variables with using force unwrapping!
-//Important it's automatically game where you are like watcher, if you want choose way to game use next following code
-// let myway: GameObj = .Paper
 
-let random = GameObj.allCases.randomElement()!
-let myChoice = GameObj.allCases.randomElement()!
+//let random = GameObj.allCases.randomElement()!
+var myChoice = Game.allCases.randomElement()!
+var computerChoice = Game.allCases.randomElement()!
 
+protocol GameRules {
+    func checkResult(me: Game, machine: Game)
+}
 
-//Implement GameProcess
-func checkResult(me: GameObj, machine: GameObj) {
-    if me == .Paper && machine == .Rock ||
-         me == .Scissors && machine == .Paper ||
-            me == .Rock && machine == .Scissors {
-                print("I win!")
-    } else if (me == machine) {
-        print("Draw!")
-    } else {
-        print("Machine win!")
+class Gaming: GameRules {
+    func checkResult(me: Game, machine: Game) {
+        if me == .Paper && machine == .Rock ||
+             me == .Scissors && machine == .Paper ||
+                me == .Rock && machine == .Scissors {
+                    print("I win!")
+        } else if (me == machine) {
+            print("Draw!")
+        } else {
+            print("Machine win!")
+        }
     }
 }
 
-
-//Show results
-print("Me:", myChoice, "\nMachine:", random)
-checkResult(me: myChoice, machine: random)
-
-//Also I tried use a original version of protocols but I couldn't use it. Below a code
-//protocol GameProcess {
-//    func checkResult()
-//}
-//
-//class Game: GameProcess {
-//    func checkResult() {
-//        print("Result is...")
-//    }
-//}
-
-
-
+var result1 = Gaming()
+result1.checkResult(me: myChoice, machine: computerChoice)
+print(myChoice)
+print(computerChoice)
